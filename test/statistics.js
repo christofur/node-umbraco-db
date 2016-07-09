37638cae9f8c45a9e38328acd9c3778038bb7a83
christofur <pop.chris@gmail.com>
2016-07-09 13:54:29 +0100

    Added additional query wrappers and tests

--- test/statistics.js
+++ test/statistics.js
@@ -18,6 +18,54 @@ describe('Statistics', function() {
 
     });
 
+    it('Should get a valid number > 0 for Member Count', function(){
+
+        return statisticsService.getCountMembers().then(function(data){
+            expect(data).to.be.above(0);
+        });
+
+    });
+
+    it('Should get a valid number > 0 for Member Group Count', function(){
+
+        return statisticsService.getCountMemberGroups().then(function(data){
+            expect(data).to.be.above(0);
+        });
+
+    });
+
+    it('Should get a valid number > 0 for User Count', function(){
+
+        return statisticsService.getCountUsers().then(function(data){
+            expect(data).to.be.above(0);
+        });
+
+    });
+
+    it('Should get a valid number > 0 for Log Entry Count', function(){
+
+        return statisticsService.getCountLogEntries().then(function(data){
+            expect(data).to.be.above(0);
+        });
+
+    });
+
+    it('Should get a valid number > 0 for Document Type Count', function(){
+
+        return statisticsService.getCountDocumentTypes().then(function(data){
+            expect(data).to.be.above(0);
+        });
+
+    });
+
+    it('Should get a valid number > 0 for Trashed Item Count', function(){
+
+        return statisticsService.getCountTrashedItems().then(function(data){
+            expect(data).to.be.above(0);
+        });
+
+    });
+
 });
 
 
